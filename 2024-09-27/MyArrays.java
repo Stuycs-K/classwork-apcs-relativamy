063f8fcb4c6af1a78f9a2a5c53645de81f6c54da
Amy Zhou <amyzhounyc@gmail.com>
2024-09-28 16:24:26 -0400

    Corrected the issues within the testing code; printed the address of the array instead of the array itself, corrected using the arrayToString code

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,25 +1,26 @@
 public class MyArrays {
         public static void main (String[] args) {
-          int[] test = new int[3];
-          System.out.println(arrayToString(test));
-		      int[] tester = {1, 2, 3};
-		      System.out.println(tester == returnCopy(tester) && tester.equals(returnCopy(tester)));
-          int[] concatTest1 = new int[] {100,100,100};
-          int[] concatTest2 = new int [] {99,99,99};
-          System.out.println(concatArray(concatTest1, concatTest2));
+        	int[] test = new int[] {1,2,3,4,5};
+		System.out.println(arrayToString(test));
+		int[] tester = new int [] {3459, -9, 444, 555, 222, 134, 556};
+		System.out.println(arrayToString(returnCopy(tester)));
+		System.out.println("Do the copied and original array hold the same address? " + (tester == returnCopy(tester))); 
+          	int[] concatTest1 = new int[] {100,100,100,99,98,99,101,105};
+          	int[] concatTest2 = new int [] {99,99,99,101,99,98,100,100,100};
+          	System.out.println(arrayToString(concatArray(concatTest1, concatTest2)));
         }
 
 
         public static String arrayToString(int[] arr) {
-          String result = "[";
-          for (int i = 0; i < arr.length; i ++) {
-            result += arr[i];
-            if (i < arr.length - 1) {
-              result += ", ";
-            }
-          }
-          result += "]";
-          return result;
+        	String result = "[";
+        	for (int i = 0; i < arr.length; i ++) {
+          	result += arr[i];
+            	if (i < arr.length - 1) {
+              		result += ", ";
+		}
+	}
+          	result += "]";
+          	return result;
         }
 
 	public static int[] returnCopy(int[] ary) {
@@ -30,14 +31,15 @@ public class MyArrays {
 		return result;
 	}
 
-  public static int[] concatArray (int[]ary1, int[]ary2) {
-    int[] result = new int [ary1.length + ary2.length];
-    for (int i = 0; i < ary1.length; i++) {
-      result[i] = ary1[i];
-    }
-    for (int i = 0; i < ary2.length; i ++) {
-      result[i+ary1.length] = ary2[i];
-    }
-    return result;
-  }
+	 public static int[] concatArray (int[]ary1, int[]ary2) {
+    		int[] result = new int [ary1.length + ary2.length];
+    		for (int i = 0; i < ary1.length; i++) {
+      			result[i] = ary1[i];
+    		}
+    		for (int i = 0; i < ary2.length; i ++) {
+      			result[i+ary1.length] = ary2[i];
+    		}
+    		return result;
+	}
 }
+
