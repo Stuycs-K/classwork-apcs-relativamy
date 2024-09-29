1b1a98926e782f5a685ba8dd0a2eb448b9a1f85f
Amy Zhou <amyzhounyc@gmail.com>
2024-09-29 19:38:31 -0400

    Added a method to test for the returnCopy code

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,15 +1,9 @@
 public class MyArrays {
         public static void main (String[] args) {
-        	int[] test = new int[] {1,2,3,4,5};
-		System.out.println(arrayToString(test));
-		int[] tester = new int [] {3459, -9, 444, 555, 222, 134, 556};
-		System.out.println(arrayToString(returnCopy(tester)));
-		System.out.println("Do the copied and original array hold the same address? " + (tester == returnCopy(tester))); 
-          	int[] concatTest1 = new int[] {100,100,100,99,98,99,101,105};
-          	int[] concatTest2 = new int [] {99,99,99,101,99,98,100,100,100};
-          	System.out.println(arrayToString(concatArray(concatTest1, concatTest2)));
-        }
-
+		System.out.println("returnCopy test:");
+		int[] tester = new int[] {1,2,3,4,5};
+        	System.out.println("Test 01: " + returnCopyTest(tester));
+	}
 
         public static String arrayToString(int[] arr) {
         	String result = "[";
@@ -30,6 +24,18 @@ public class MyArrays {
 		}
 		return result;
 	}
+
+        public static String returnCopyTest(int[] arr) {
+                if (arrayToString(arr).equals(arrayToString(arr)) && arr != returnCopy(arr)) {
+                        return "Pass";
+                }
+		else if (arrayToString(arr).equals(arrayToString(arr)) && arr == returnCopy(arr)) {
+			return "Failed, the copied and original string hold the same address";
+		}
+                else {
+                        return "Failed, expected " + arr.toString() +"  but value was " +  arrayToString(arr);
+                }
+        }
 
 	 public static int[] concatArray (int[]ary1, int[]ary2) {
     		int[] result = new int [ary1.length + ary2.length];
