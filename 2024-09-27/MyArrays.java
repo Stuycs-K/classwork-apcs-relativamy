9e801aa9d33d871ffdfe2cddb2518c004bab8b71
Amy Zhou <amyzhounyc@gmail.com>
2024-09-29 20:43:22 -0400

    Added test cases for concatArrays and returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -3,9 +3,34 @@ public class MyArrays {
 		System.out.println("returnCopy test:");
 		int[] copyTester = new int[] {1,2,3,4,5};
         	System.out.println("Test 01: " + returnCopyTest(copyTester));
+                copyTester = new int[] {4,5,7,18,3,-49};
+                System.out.println("Test 02: " + returnCopyTest(copyTester));
+                copyTester = new int[] {-101,23,45,3,2,1,566,79};           
+                System.out.println("Test 02: " + returnCopyTest(copyTester));
+                copyTester = new int[] {100,100,100,100,100};           
+                System.out.println("Test 03: " + returnCopyTest(copyTester));
+                copyTester = new int[] {99,99,99,99,99,100,999999};           
+                System.out.println("Test 04: " + returnCopyTest(copyTester));
+                copyTester = new int[] {11,22,33,44,55,66,77,88,99};
+                System.out.println("Test 05: " + returnCopyTest(copyTester));
+		System.out.println(" ");
+		System.out.println("concatArray test:");
 		int[] concatTester1 = new int[] {100,99,988};
 		int[] concatTester2 = new int[] {101,98,223,222};
 		System.out.println("Test 01: " + concatArrayTest(concatTester1, concatTester2, 0));
+		concatTester1 = new int[] {1,2,3,4,5};
+		concatTester2 = new int[] {6,7,8,9,10};
+                System.out.println("Test 02: " + concatArrayTest(concatTester1, concatTester2, 1));
+                concatTester1 = new int[] {};
+                concatTester2 = new int[] {};
+                System.out.println("Test 03: " + concatArrayTest(concatTester1, concatTester2, 2));
+                concatTester1 = new int[] {-1,-7,1,3,5};      
+                concatTester2 = new int[] {44,55,21,3,-5};          
+                System.out.println("Test 04: " + concatArrayTest(concatTester1, concatTester2, 3));
+                concatTester1 = new int[] {1};      
+                concatTester2 = new int[] {2};          
+                System.out.println("Test 05: " + concatArrayTest(concatTester1, concatTester2, 4));
+
 	}
 
         public static String arrayToString(int[] arr) {
@@ -51,7 +76,13 @@ public class MyArrays {
     		return result;
         }
 	public static String concatArrayTest (int[]ary1, int[]ary2, int n) {
-		String[] results = new String[] {"[100, 99, 988, 101, 98, 223, 222]"};
+		String[] results = new String[] {
+			"[100, 99, 988, 101, 98, 223, 222]", 
+			"[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", 
+			"[]", 
+			"[-1, -7, 1, 3, 5, 44, 55, 21, 3, -5]", 
+			"[1, 2]"
+		};
 		String expected = results[n];
 		String actual = arrayToString(concatArray(ary1,ary2));
 		if (expected.equals(actual)) {
