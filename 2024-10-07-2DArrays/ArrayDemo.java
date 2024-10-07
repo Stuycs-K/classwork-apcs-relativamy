e66944bf3b49900d7291c6b9c39e236012edb569
Amy Zhou <amyzhounyc@gmail.com>
2024-10-07 10:12:52 -0400

    Copied the replaceNegative into the classwork file

--- 2024-10-07-2DArrays/ArrayDemo.java
+++ 2024-10-07-2DArrays/ArrayDemo.java
@@ -71,8 +71,20 @@ public class ArrayDemo{
   //that negative with the value 1
   //-All other negatives replace with 0
   public static void replaceNegative(int[][] vals){
-
+    for (int i = 0; i < vals.length; i ++) {
+      for (int j = 0; j < vals[i].length; j ++) {
+        if (vals[i][j] < 0) {
+          if (i == j) {
+            vals[i][j] = 1;
+          }
+          else {
+            vals[i][j] = 0;
+          }
+        }
+      }
+    }
   }
+
 
   //4. Make a copy of the given 2d array.
   //When testing : make sure that changing the original does NOT change the copy.
