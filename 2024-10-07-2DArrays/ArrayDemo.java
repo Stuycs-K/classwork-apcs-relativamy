7447d53c218379a942186eeae4fab36baefbdb57
Amy Zhou <amyzhounyc@gmail.com>
2024-10-07 10:10:37 -0400

    Corrected the arrToString function (caused by incorrect copy and paste) and wrote the countZeros2D method

--- 2024-10-07-2DArrays/ArrayDemo.java
+++ 2024-10-07-2DArrays/ArrayDemo.java
@@ -12,27 +12,41 @@ public class ArrayDemo{
   public static String arrToString(int[]ary){
     String result = "[";
     for (int i = 0; i < ary.length; i ++) {
-      result += arrToString(ary[i]);
+      result += ary[i];
       if (i < ary.length - 1) {
          result += ", ";
        }
      }
+     return result;
   }
 
   //The name of different methods can be the same,
   //as long as the parameters are different! (type and/or quantity must be different)
   //Pro tip: you should be using your 1D arrToString in this method!
   public static String arrToString(int[][]ary){
-    if (arrToString(ary).equals(Arrays.deepToString(ary))) {
-      return "Pass";
-    } else {
-      return "Failed, expected " + Arrays.deepToString(ary) + " but recieved " + arrToString(ary);
+    //this should use arrToString(int[])
+    String result = "[";
+    for (int i = 0; i < ary.length; i ++) {
+      result += arrToString(ary[i]);
+      if (i < ary.length - 1) {
+	       result += ", ";
+      }
     }
+    result += "]";
+    return result;
   }
 
   //1. Calculate and return how many elements equal zero in the 2D array.
   public static int countZeros2D(int[][] nums){
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i ++) {
+      for (int j = 0; j < nums[i].length; j ++) {
+        if (nums[i][j] == 0) {
+          sum ++;
+        }
+      }
+    }
+    return sum;
   }
 
   //2. Calculate the sum of a 2d array
