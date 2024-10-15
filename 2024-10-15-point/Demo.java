7eae7e1640fc7f30ff58ab821ad61ef12ffe19ca
Amy Zhou <amyzhounyc@gmail.com>
2024-10-15 13:11:13 -0400

    added the three points of a triangle and printed their side lengths

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -12,17 +12,35 @@ public class Demo {
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
+    // toString test cases
     System.out.println( p1);
     System.out.println( p2);
     System.out.println( p3);
+
+    // distance test cases with p1 and p2
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+
+    // distance test cases with p1 and p3
     System.out.println( distance(p1,p3));
     System.out.println( Point.distance(p1,p3));
     System.out.println( p1.distanceTo(p3));
+
+    // distance test cases with p2 and p3
     System.out.println( distance(p2,p3));
     System.out.println( Point.distance(p2,p3));
     System.out.println( p2.distanceTo(p3));
+
+    // equilateral triangle points
+    Point t1 = new Point(0,0);
+    Point t2 = new Point(1,0);
+    Point t3 = new Point(0.5,Math.sqrt(3));
+
+    // equilateral triangle side length tests
+    System.out.println( distance(t1,t2));
+    System.out.println( distance(t1,t3));
+    System.out.println( distance(t2,t3));
+
   }
 }
