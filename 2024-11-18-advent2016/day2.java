280b0b5483da4b003a21e9d57dd02883cd64f311
Amy Zhou <amyzhounyc@gmail.com>
2024-11-20 10:08:04 -0500

    parse each line of code

---
+++ 2024-11-18-advent2016/day2.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import javax.sound.sampled.Line;
+import java.util.Arrays;
+
+public class day2 {
+  public static void main(String[] args){
+    System.out.println(Arrays.toString(parse("day2.txt")));
+    //System.out.println(solve(parse("day1.txt")));
+  }
+  public static String[] parse(String filename){
+    ArrayList<String> vals = new ArrayList<String>();
+    try {
+      File file = new File (filename);
+      Scanner info = new Scanner (file);
+      while (info.hasNext()) {
+        vals.add(info.next());
+      }
+      String[] val = new String[vals.size()];
+      for (int i = 0; i < vals.size(); i ++) {
+        val[i] = vals.get(i);
+      }
+      return val;
+    } catch (FileNotFoundException e) {
+      return new String[0];
+    }
+  }
+
+  public static int solve (String[] data) {
+    int x=0,y=0,dir=0;
+    int[][] offset = {{0,1},{1,0},{0,-1},{-1,0}};
+    for (int i = 0; i < data.length; i++) {
+      if (data[i].charAt(0) == 'R') {
+        dir ++;
+      }
+      else {
+        dir --;
+      }
+      int steps = Integer.parseInt(data[i].substring(1));
+      dir = (dir + 4) % 4;
+      x = offset[dir][0] * steps;
+      y = offset[dir][1] * steps;
+    }
+    return 0;
+  }
+}
