fb0c60b11fa02db09114a14c3e9199d597eefa58
Amy Zhou <amyzhounyc@gmail.com>
2024-12-05 03:52:33 -0500

    dividing line impl

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,30 +1,31 @@
 public class Driver {
   public static void main (String[] args) {
-    border(80,30);
-    draw();
+    border(80,30,"-");
+    Text.go(31,1);
+    numbers();
+    border(80,1,"*");
+    System.out.println(Text.SHOW_CURSOR);
   }
-  public static void border (int width, int height) {
+  public static void border (int width, int height, String borderText) {
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
     Text.color (Text.RED, Text.background(Text.CYAN));
     for (int i = 1; i <= width; i ++) {
       Text.go(1,i);
-      System.out.println(" ");
+      System.out.println(borderText);
     }
     for (int i = 1; i <= width; i++) {
       Text.go(height, i);
-      System.out.println(" ");
+      System.out.println(borderText);
     }
     for (int j = 2; j < height; j ++) {
       Text.go(j, 1);
-      System.out.print(" ");
+      System.out.print(borderText);
       Text.go(j, width);
-      System.out.println(" ");
+      System.out.println(borderText);
     }
-    Text.go(height+1,1);
-    System.out.print(Text.RESET);
   }
-  public static void draw() {
+  public static void numbers() {
     int[] nums = new int[3];
     for (int i = 0; i < 3; i ++) {
       nums[i] = (int)(Math.random() * 100);
