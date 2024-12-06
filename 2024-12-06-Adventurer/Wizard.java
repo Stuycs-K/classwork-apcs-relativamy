888ca1c1d5ecad1d7548aa2a66339a8db8d6847f
Amy Zhou <amyzhounyc@gmail.com>
2024-12-06 09:55:23 -0500

    support and attack methods impl

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,9 +1,35 @@
-public class Wizard extends Adventurer {
+import java.util.Random;
+public abstract class Wizard extends Adventurer {
+  private int special = 0;
   public Wizard (String name) {
     super(name);
   }
   public Wizard (String name, int hp) {
     super(name, hp);
   }
-  public
+  public String getSpecialName() {
+    return "mana";
+  }
+  public int getSpecial() {
+    return special;
+  }
+  public void setSpecial(int n) {
+    special = n;
+  }
+  public int getSpecialMax() {
+    return 100;
+  }
+  public String attack (Adventurer other) {
+    other.applyDamage(10);
+    return "damaged " + other.getName() + " by 10 hp";
+  }
+  public String support (Adventurer other) {
+    other.setHP(other.getHP() + 10);
+    return "supported " + other.getName() + " by 10 hp";
+  }
+  public String specialAttack (Adventurer other) {
+    other.setSpecial(other.getSpecial() - 15);
+    other.applyDamage(20);
+    return "special attack! damaged " + other.getName() + " -15 special and -20 HP";
+  }
 }
