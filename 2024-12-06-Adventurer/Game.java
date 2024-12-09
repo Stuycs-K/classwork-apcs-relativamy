baa4604c6da4b7a935f86903f96266bd40b55de8
Amy Zhou <amyzhounyc@gmail.com>
2024-12-09 11:28:46 -0500

    impl input for game

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 public class Game {
   public static void main (String[] args) {
     Scanner userInput = new Scanner(System.in);
-    boolean flag = true;
-    while (flag) {
-      System.out.println("Enter player name:");
-      String userName = userInput.nextLine();
-      Adventurer player = new Wizard(userName, 100);
-      System.out.println("Enter enemy name:");
-      String userName2 = userInput.nextLine();
-      Adventurer enemy = new CodeWarrior(userName2, 100);
-      System.out.println("Player: " + player.getName() +
-        ", " + player.getHP() + "/" + player.getmaxHP() + " HP" +
-        ", "  + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName() + " ");
-        System.out.println("Enemy: " + enemy.getName() +
-          ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP" +
-          ", "  + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
-      String user = userInput.nextLine();
-      if (user.equalsTo("quit")) {
+    System.out.print("Enter player name: ");
+    String userName = userInput.nextLine();
+    Adventurer player = new Wizard(userName, 100);
+    System.out.print("Enter enemy name: ");
+    String userName2 = userInput.nextLine();
+    Adventurer enemy = new CodeWarrior(userName2, 100);
+    System.out.println("Player: " + player.getName() +
+      ", " + player.getHP() + "/" + player.getmaxHP() + " HP" +
+      ", "  + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName() + " ");
+      System.out.println("Enemy: " + enemy.getName() +
+        ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP" +
+        ", "  + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+    while (true) {
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String input = userInput.nextLine();
+      if (input.equals("a")) {
+        System.out.println(player.attack(enemy));
+      }
+      else if (input.equals("sp")) {
+        System.out.println(player.specialAttack(enemy));
+      }
+      else if (input.equals("su")) {
+        System.out.println(player.support());
+      }
+      else if (input.equals("quit")) {
         return;
+      }
+      else {
+        System.out.println("Incorrect input. Type: (a)ttack / (sp)ecial / (su)pport / quit");
       }
     }
   }
