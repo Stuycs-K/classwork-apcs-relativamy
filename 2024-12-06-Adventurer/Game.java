c38ccb489ee588143d467e1a8b8e39797dfbdd20
Amy Zhou <amyzhounyc@gmail.com>
2024-12-09 10:14:03 -0500

    changed loop code

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 public class Game {
   public static void main (String[] args) {
     Scanner userInput = new Scanner(System.in);
-    System.out.println("Enter player name:");
-    String userName = userInput.nextLine();
-    Adventurer player = new Wizard(userName, 100);
-    System.out.println("Enter enemy name:");
-    String userName2 = userInput.nextLine();
-    Adventurer enemy = new CodeWarrior(userName2, 100);
-    System.out.println("Player: " + player.getName() +
-      ", " + player.getHP() + "/" + player.getmaxHP() + " HP" +
-      ", "  + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName() + " ");
-      System.out.println("Enemy: " + enemy.getName() +
-        ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP" +
-        ", "  + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+    boolean flag = true;
+    while (flag) {
+      System.out.println("Enter player name:");
+      String userName = userInput.nextLine();
+      Adventurer player = new Wizard(userName, 100);
+      System.out.println("Enter enemy name:");
+      String userName2 = userInput.nextLine();
+      Adventurer enemy = new CodeWarrior(userName2, 100);
+      System.out.println("Player: " + player.getName() +
+        ", " + player.getHP() + "/" + player.getmaxHP() + " HP" +
+        ", "  + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName() + " ");
+        System.out.println("Enemy: " + enemy.getName() +
+          ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP" +
+          ", "  + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+      String user = userInput.nextLine();
+      if (user.equalsTo("quit")) {
+        return;
+      }
+    }
   }
 }
